f80a206933ae2750bf3e4f000f737d02d1c34e7c
Kenny Garcia Benitez <kennylpgc@gmail.com>
2023-03-23 09:42:24 +0000

    eliminado componentes

--- src/components/Carrito.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const Carrito = () => {
-  return (
-    <div><h1>Carritoo</h1></div>
-  )
-}
-
-export default Carrito
--- src/components/Producto.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const Producto = () => {
-  return (
-    <div><h1>Productoo</h1></div>
-  )
-}
-
-export default Producto
